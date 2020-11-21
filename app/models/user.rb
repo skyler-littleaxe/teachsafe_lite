1311a07a71acbb22b7f7e30a9c1d8dba4c35c070
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2020-11-21 04:05:46 -0600

    added :omniauthable and others to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
   has_many :students
   has_one :teacher
   has_many :students_as_teacher, through: :teacher, source: :students
