7a7b23f32ac58853c1955b06a2552fe46b53e3fa
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2020-11-23 02:37:55 -0600

    generated Teacher resources with belongs_to User association

---
+++ db/migrate/20201123083501_create_teachers.rb
@@ -0,0 +1,13 @@
+class CreateTeachers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :teachers do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :phone_number
+      t.belongs_to :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
