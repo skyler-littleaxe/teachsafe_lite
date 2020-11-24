65cae77b64a820518b9d065908fd250530dfdccd
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2020-11-23 22:23:16 -0600

    updated associations for Student and Teacher

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,6 @@
 class Student < ApplicationRecord
   belongs_to :user
+  has_many :student_teachers
+	has_many :teachers, through: :student_teachers
+	has_many :current_teachers,-> {where(student_teachers: {year: Time.now.year})}, through: :student_teachers
 end
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,3 +1,6 @@
 class Teacher < ApplicationRecord
   belongs_to :user
+  has_many :student_teachers
+	has_many :students, through: :student_teachers
+	has_many :current_students,-> {where(student_teachers: {year: Time.now.year})}, through: :student_teachers
 end
